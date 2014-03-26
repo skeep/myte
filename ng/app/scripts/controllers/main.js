eb6f8aac3be5b44a61dad15a99f9cfc38e68b16c
Suman Paul <sumank.paul@gmail.com>
2014-03-26 17:49:11 +0530

    upadtes

--- ng/app/scripts/controllers/main.js
+++ ng/app/scripts/controllers/main.js
@@ -1,10 +1,43 @@
-'use strict';
+angular.module('ngApp').controller('MainCtrl', function ($scope) {
 
-angular.module('ngApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  'use strict';
+
+  var Expenses = Parse.Object.extend("expenses");
+  var expense = new Expenses();
+  var query = new Parse.Query(Expenses);
+
+  $scope.expenseList = [];
+  query.find({
+    success : function(results){
+      console.log(results);
+      $scope.expenseList = results;
+      $scope.$apply();
+    },
+    error : function(error){
+      console.log(error);
+    }
   });
+
+  $scope.save = function () {
+    expense.save({
+      location: 'Bangalore',
+      type: 'Travel',
+      amount: 500,
+      currency: 'INR'
+    }, {
+      success: function (expense) {
+        // The object was saved successfully.
+        console.log(expense);
+      },
+      error: function (expense, error) {
+        console.log(error);
+        // The save failed.
+        // error is a Parse.Error with an error code and description.
+      }
+    });
+  };
+
+
+
+
+});
--- parse/public/script.js
+++ parse/public/script.js
@@ -11,3 +11,17 @@ Parse.Cloud.run('hello', {}, {
   error: function(error) {
   }
 });
+
+
+var myInput = document.getElementById('myFileInput');
+
+function sendPic() {
+  var file = myInput.files[0];
+
+  alert('file sent');
+  // Send file here either by adding it to a `FormData` object
+  // and sending that via XHR, or by simply passing the file into
+  // the `send` method of an XHR instance.
+}
+
+myInput.addEventListener('change', sendPic, false);
